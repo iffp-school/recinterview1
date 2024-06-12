145f5c264f4b3c3bab76528a78497c72388fe7bc
mouad <mouaadsellak123@gmail.com>
2024-06-12 14:25:21 +0200

    add react & tailwind

---
+++ resources/js/components/HelloReact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HelloReact() {
+    return (
+        <div className="p-4 bg-blue-500 text-white">
+            <h1>Hello, React with Tailwind CSS!</h1>
+        </div>
+    );
+}
+
+export default HelloReact;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.jsx'],
+      refresh: true,
+    }),
+    react(),
+  ],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+  },
 });
